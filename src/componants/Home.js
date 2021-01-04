ab9258af754b077f85e06237c035db922de98f93
Raghav Chinda <raghav.chinda@daffodilsw.com>
2021-01-04 17:20:04 +0530

    user profiles added

--- src/componants/Home.js
+++ src/componants/Home.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../App.css';
+import axios from 'axios';
 
 export default function Home() {
+
+    const [candidates , setCandidates]= useState([]);
+
+    const getData= async ()=>{ 
+
+        let response= await axios.get('https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json');
+        setCandidates(response.data);
+    }
+
+    useEffect(()=>{
+        getData();
+    },[]);
+    console.log(candidates);
+
     return (
         <div className="body">
         <div className="Header">
@@ -9,7 +24,17 @@ export default function Home() {
         <div className="UserProfileLogo"><img alt="User profile" src="ic_user.png"></img></div>
         </div>
         <div className="main">
-            
+        {
+         candidates.map((candidate)=>{
+            return <div className="Container">
+                 <div className="userImage"><img alt="User" src={candidate.Image}></img></div>
+                 <div className="userDetails">
+                 <div>User Name: ${candidate.name}</div>
+                 <div>User ID: ${candidate.id}</div>
+                 </div>
+            </div>
+         })
+        }  
         </div>
         <div className="footer">
             <span className="copyright">©Job Portal 2021</span>
