407df495223b273d759afe0992421482e123b012
Raghav Chinda <raghav.chinda@daffodilsw.com>
2021-01-04 16:10:36 +0530

    second commit

--- src/componants/Home.js
+++ src/componants/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="body">
         <div className="Header">
         <div className="Title">Job Portal</div>
-        <div className="UserProfileLogo"><img src="ic_user.png"></img></div>
+        <div className="UserProfileLogo"><img alt="User profile" src="ic_user.png"></img></div>
         </div>
         <div className="main">
             
