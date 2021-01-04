1f4daa32f1d02e8603816df0aeb49b76457445e1
Raghav Chinda <raghav.chinda@daffodilsw.com>
2021-01-04 19:57:18 +0530

    User profiles are updated

--- src/componants/CandidateProfile.js
+++ src/componants/CandidateProfile.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
-export default function CandidateProfile(candidates) {
+export default function CandidateProfile({candidates}) {
+     
+    const [candidate, setCandidate]=useState({});
+    const {id}=useParams();
+    
+    useEffect(()=>{
+        const candi=candidates.find((candidate)=> candidate.id === id);
+        setCandidate(candi);
+    },[]);
     return (
-        <div>
-            Hello!!!!!!
+        <div className="profileContainer">
+            <div className="profilePicture" >
+            <div><img src={candidate.Image} alt="profile picture"></img></div>
+            </div>
+            <div className="profileDetails">
+                <div className="userName">{candidate.name}</div>
+                <div className="userName">{candidate.id}</div>
+            </div>
         </div>
     )
 }
